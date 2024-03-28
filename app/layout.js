9b63033f783bd673b87fd8ad678769f1c3ad4d17
Darwin Camahalan <120079195+DarwinCamahalan@users.noreply.github.com>
2024-03-28 15:45:09 +0800

    Added Next.js 14.1.4 Framework for Web App

---
+++ app/layout.js
@@ -0,0 +1,21 @@
+import Script from 'next/script';
+
+export const metadata = {
+  title: "Medical Consultation",
+  description: "An Online Medical Consultation that can determine patients heart rate per minute and more.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        {/* Other meta tags, stylesheets, etc. can go here */}
+      </head>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
