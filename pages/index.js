4e6f6d95e09b24fb4a547615263d74167ea10701
Darwin Camahalan <120079195+DarwinCamahalan@users.noreply.github.com>
2024-04-01 13:00:43 +0800

    Added Page Route for BPM Detection

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,6 @@
-import { useEffect, useState } from 'react';
 import Head from 'next/head';
-import io from 'socket.io-client';
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  const [bpm, setBpm] = useState(0);
-
-  useEffect(() => {
-    const socket = io('http://localhost:5000');
-
-    // Listen for BPM updates from the WebSocket server
-    socket.on('bpm_update', ({ bpm }) => {
-      setBpm(bpm);
-    });
-
-    return () => {
-      socket.disconnect();
-    };
-  }, []);
-
   return (
     <>
       <Head>
@@ -28,11 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-        <p>BPM: {bpm}</p>
-        <div>
-        <iframe src="http://127.0.0.1:5000/face_detection" width="650" height="550" frameBorder="0"></iframe>
-        <iframe src="http://127.0.0.1:5000/bpm_detection" width="320" height="240" frameBorder="0"></iframe>
-        </div>
+        Home Page
       </div>
     </>
   );
