752d0b7c7abb0398eb665fa542f293658c6d2a32
Darwin Camahalan <120079195+DarwinCamahalan@users.noreply.github.com>
2024-04-07 10:49:13 +0800

    Added different graphs for each tab

--- components/DisplayGraphs.js
+++ components/DisplayGraphs.js
@@ -1,20 +1,22 @@
-import styles from './graphs.module.scss'
 import React, { useState, useEffect } from 'react';
 import { db } from '../firebaseConfig';
 import { collection, getDocs, query, where } from 'firebase/firestore';
 import { Chart, LinearScale, PointElement, Tooltip, Legend, TimeScale } from 'chart.js';
-import {motion} from 'framer-motion'
-Chart.register(LinearScale, PointElement, Tooltip, Legend, TimeScale);
 import { Scatter } from 'react-chartjs-2';
 import Cookies from 'js-cookie';
 import moment from 'moment';
 import 'chartjs-adapter-moment';
+import styles from './graphs.module.scss';
+import { motion } from 'framer-motion';
+
+Chart.register(LinearScale, PointElement, Tooltip, Legend, TimeScale);
 
 const DisplayGraphs = ({ showGraphs, setShowGraphs }) => {
     const [selectedDate, setSelectedDate] = useState('');
     const [bpmData, setBpmData] = useState({});
     const [availableDates, setAvailableDates] = useState([]);
     const userEmail = Cookies.get('userEmail');
+    const [timeRange, setTimeRange] = useState('daily');
 
     useEffect(() => {
         const fetchPatientData = async () => {
@@ -53,15 +55,219 @@ const DisplayGraphs = ({ showGraphs, setShowGraphs }) => {
         }
     }, [userEmail]);
 
-
     const handleDateChange = event => {
         setSelectedDate(event.target.value);
+    };
+
+    const handleTimeRangeChange = range => {
+        setTimeRange(range);
     };
 
     const handleModalClick = event => {
         if (event.target.classList.contains(styles.modalBackground)) {
             setShowGraphs(false);
         }
+    };
+
+    const renderGraph = () => {
+        if (selectedDate && bpmData[selectedDate] !== undefined) {
+            switch (timeRange) {
+                case 'daily':
+                    return renderDailyGraph();
+                case 'weekly':
+                    return renderWeeklyGraph();
+                case 'monthly':
+                    return renderMonthlyGraph();
+                default:
+                    return <p className={styles.noData}>Invalid time range.</p>;
+            }
+        } else {
+            return <p className={styles.noData}>No available data for this date.</p>;
+        }
+    };
+
+    const renderDailyGraph = () => {
+        return (
+            <Scatter
+                style={{ padding: '10px 20px' }}
+                data={{
+                    datasets: [
+                        {
+                            label: 'BPM Data',
+                            data: Object.entries(bpmData[selectedDate]).map(([time, { bpmValue }]) => ({
+                                x: moment(time, 'HH:mm:ss').toDate(),
+                                y: bpmValue || 0,
+                            })),
+                            pointRadius: 6,
+                            pointBackgroundColor: ctx => {
+                                const value = ctx.dataset.data[ctx.dataIndex].y;
+                                if (value >= 0 && value <= 40) {
+                                    return 'rgb(162, 255, 0)';
+                                } else if (value >= 41 && value <= 70) {
+                                    return 'rgb(82, 245, 0)';
+                                } else if (value >= 71 && value <= 90) {
+                                    return 'rgb(255, 0, 195)';
+                                } else {
+                                    return 'rgb(255, 0, 5)';
+                                }
+                            },
+                        },
+                    ],
+                }}
+                options={{
+                    scales: {
+                        x: {
+                            type: 'time',
+                            time: {
+                                unit: 'minute',
+                            },
+                            title: {
+                                display: true,
+                                text: 'Time',
+                            },
+                        },
+                        y: {
+                            title: {
+                                display: true,
+                                text: 'BPM Value',
+                            },
+                        },
+                    },
+                }}
+            />
+        );
+    };
+
+    const renderWeeklyGraph = () => {
+        // Logic to calculate weekly average BPM
+        const weeklyData = calculateWeeklyData(selectedDate);
+
+        if (weeklyData) {
+            return (
+                <Scatter
+                    style={{ padding: '10px 20px' }}
+                    data={{
+                        datasets: [
+                            {
+                                label: 'Weekly BPM Data',
+                                data: weeklyData.map(({ date, avgBpm }) => ({
+                                    x: moment(date, 'MM/DD/YYYY').toDate(),
+                                    y: avgBpm,
+                                })),
+                                pointRadius: 6,
+                                pointBackgroundColor: 'rgb(0, 0, 255)',
+                            },
+                        ],
+                    }}
+                    options={{
+                        scales: {
+                            x: {
+                                type: 'time',
+                                time: {
+                                    unit: 'day',
+                                },
+                                title: {
+                                    display: true,
+                                    text: 'Date',
+                                },
+                            },
+                            y: {
+                                title: {
+                                    display: true,
+                                    text: 'Average BPM Value',
+                                },
+                            },
+                        },
+                    }}
+                />
+            );
+        } else {
+            return <p className={styles.noData}>No available data for this week.</p>;
+        }
+    };
+
+    const renderMonthlyGraph = () => {
+        // Logic to calculate monthly average BPM
+        const monthlyData = calculateMonthlyData(selectedDate);
+
+        if (monthlyData) {
+            return (
+                <Scatter
+                    style={{ padding: '10px 20px' }}
+                    data={{
+                        datasets: [
+                            {
+                                label: 'Monthly BPM Data',
+                                data: monthlyData.map(({ date, avgBpm }) => ({
+                                    x: moment(date, 'MM/DD/YYYY').toDate(),
+                                    y: avgBpm,
+                                })),
+                                pointRadius: 6,
+                                pointBackgroundColor: 'rgb(255, 0, 0)',
+                            },
+                        ],
+                    }}
+                    options={{
+                        scales: {
+                            x: {
+                                type: 'time',
+                                time: {
+                                    unit: 'day',
+                                },
+                                title: {
+                                    display: true,
+                                    text: 'Date',
+                                },
+                            },
+                            y: {
+                                title: {
+                                    display: true,
+                                    text: 'Average BPM Value',
+                                },
+                            },
+                        },
+                    }}
+                />
+            );
+        } else {
+            return <p className={styles.noData}>No available data for this month.</p>;
+        }
+    };
+
+    const calculateWeeklyData = (selectedDate) => {
+        const startOfWeek = moment(selectedDate, 'MM/DD/YYYY').startOf('week').format('MM/DD/YYYY');
+        const endOfWeek = moment(selectedDate, 'MM/DD/YYYY').endOf('week').format('MM/DD/YYYY');
+        const weeklyData = [];
+
+        for (let date = startOfWeek; moment(date, 'MM/DD/YYYY').isSameOrBefore(endOfWeek); date = moment(date, 'MM/DD/YYYY').add(1, 'days').format('MM/DD/YYYY')) {
+            const dailyData = bpmData[date];
+            if (dailyData) {
+                const entries = Object.values(dailyData);
+                const totalBpm = entries.reduce((acc, entry) => acc + entry.bpmValue, 0);
+                const avgBpm = totalBpm / entries.length;
+                weeklyData.push({ date, avgBpm });
+            }
+        }
+
+        return weeklyData;
+    };
+
+    const calculateMonthlyData = (selectedDate) => {
+        const startOfMonth = moment(selectedDate, 'MM/DD/YYYY').startOf('month').format('MM/DD/YYYY');
+        const endOfMonth = moment(selectedDate, 'MM/DD/YYYY').endOf('month').format('MM/DD/YYYY');
+        const monthlyData = [];
+
+        for (let date = startOfMonth; moment(date, 'MM/DD/YYYY').isSameOrBefore(endOfMonth); date = moment(date, 'MM/DD/YYYY').add(1, 'days').format('MM/DD/YYYY')) {
+            const dailyData = bpmData[date];
+            if (dailyData) {
+                const entries = Object.values(dailyData);
+                const totalBpm = entries.reduce((acc, entry) => acc + entry.bpmValue, 0);
+                const avgBpm = totalBpm / entries.length;
+                monthlyData.push({ date, avgBpm });
+            }
+        }
+
+        return monthlyData;
     };
 
     return (
@@ -79,9 +285,9 @@ const DisplayGraphs = ({ showGraphs, setShowGraphs }) => {
                             transition={{ duration: 0.5 }}>
 
                             <div className={styles.tabs}>
-                                <span>Daily</span>
-                                <span>Weekly</span>
-                                <span>Monthly</span>
+                                <span onClick={() => handleTimeRangeChange('daily')}>Daily</span>
+                                <span onClick={() => handleTimeRangeChange('weekly')}>Weekly</span>
+                                <span onClick={() => handleTimeRangeChange('monthly')}>Monthly</span>
                             </div>
 
                             <select onChange={handleDateChange} className={styles.selectDate}>
@@ -93,57 +299,7 @@ const DisplayGraphs = ({ showGraphs, setShowGraphs }) => {
                                 ))}
                             </select>
 
-                            {selectedDate && bpmData[selectedDate] !== undefined ? (
-                                <Scatter
-                                    style={{padding: '10px 20px'}}
-                                    data={{
-                                        datasets: [
-                                            {
-                                                label: 'BPM Data',
-                                                data: Object.entries(bpmData[selectedDate]).map(([time, { bpmValue }]) => ({
-                                                    x: moment(time, 'HH:mm:ss').toDate(),
-                                                    y: bpmValue || 0,
-                                                })),
-                                                pointRadius: 6,
-                                                pointBackgroundColor: ctx => {
-                                                    const value = ctx.dataset.data[ctx.dataIndex].y;
-                                                    if (value >= 0 && value <= 40) {
-                                                        return 'rgb(162, 255, 0)';
-                                                    } else if (value >= 41 && value <= 70) {
-                                                        return 'rgb(82, 245, 0)';
-                                                    } else if (value >= 71 && value <= 90) {
-                                                        return 'rgb(255, 0, 195)';
-                                                    } else {
-                                                        return 'rgb(255, 0, 5)';
-                                                    }
-                                                },
-                                            },
-                                        ],
-                                    }}
-                                    options={{
-                                        scales: {
-                                            x: {
-                                                type: 'time',
-                                                time: {
-                                                    unit: 'minute',
-                                                },
-                                                title: {
-                                                    display: true,
-                                                    text: 'Time',
-                                                },
-                                            },
-                                            y: {
-                                                title: {
-                                                    display: true,
-                                                    text: 'BPM Value',
-                                                },
-                                            },
-                                        },
-                                    }}
-                                />
-                            ) : (
-                                <p className={styles.noData}>No available data for this date.</p>
-                            )}
+                            {renderGraph()}
                         </motion.div>
                     </div>
                 </motion.div>
